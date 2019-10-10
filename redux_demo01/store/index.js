57918016ad1c7ebed61268f6b952f477029b7903
lmxyjy <yjy15680489038@163.com>
2019-10-10 17:01:09 +0800

    .gitignore

--- redux_demo01/store/index.js
+++
@@ -1,9 +0,0 @@
-import {createStore} from 'redux';
-const { composeWithDevTools } = require('redux-devtools-extension');
-import reducer from './reducer';
-
-const store = createStore(
-    reducer,
-    composeWithDevTools()
-    );
-export default store;
